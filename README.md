4d074e3975873ffa341a038adb8c9d0e9ffdbc9d
Tommy Crews <tommy@crews.dev>
2023-08-30 20:05:50 -0500

    Added first script.

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# local-config
+# Local Configuration Repo
+
+This repo contains scripts and other useful things for local workstations and other personal machines.
---
+++ macos/mbp-arm64/workstation-setup.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+# Initial dev workstation setup script.
+
+# Install Homebrew if not already installed
+if ! command -v brew &> /dev/null; then
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    (echo; echo 'eval "$(/opt/homebrew/bin/brew shellenv)"') >> /Users/tommyc/.zprofile
+    eval "$(/opt/homebrew/bin/brew shellenv)"
+fi
+
+# Add tap required for things like Visual Studio
+brew tap homebrew/cask-versions
+
+# Install software using Homebrew (where available)
+brew install --cask 1password
+brew install --cask android-studio
+brew install --cask arc
+brew install --cask charles
+brew install --cask daisydisk
+brew install --cask discord
+brew install --cask docker
+brew install gifski
+brew install --cask gimp
+brew install --cask gitkraken
+brew install --cask google-chrome
+brew install --cask google-drive
+brew install --cask inkscape
+brew install --cask logi-options-plus
+brew install mas
+brew install --cask messenger
+brew install --cask microsoft-remote-desktop
+brew install --cask microsoft-teams
+brew install --cask parallels
+brew install --cask postman
+brew install --cask powershell
+brew install --cask slack
+brew install --cask visual-studio
+brew install --cask visual-studio-code
+brew install --cask warp
+
+# Install software using Mac App Store
+mas install 497799835  # Xcode
+mas install 441258766  # Magnet
+mas install 1561788435 # Usage
+
+# Install Xcode command line tools if necessary
+xcode-select --install || true
+
+# Restart machine
+echo "Software installation completed."
