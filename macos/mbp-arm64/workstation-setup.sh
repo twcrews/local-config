e333a5e65995ba8efe9946bf92fc2877d16535ba
Tommy Crews <tommy@crews.dev>
2023-09-19 09:23:36 -0500

    Added stuff to install.

--- macos/mbp-arm64/workstation-setup.sh
+++ macos/mbp-arm64/workstation-setup.sh
@@ -15,13 +15,16 @@ brew tap homebrew/cask-versions
 brew install --cask 1password
 brew install --cask android-studio
 brew install --cask arc
-brew install --cask charles
+brew install --cask charles-beta
 brew install --cask daisydisk
 brew install --cask discord
 brew install --cask docker
+brew install --cask dotnet-sdk
 brew install gifski
 brew install --cask gimp
+brew install git-lfs
 brew install --cask gitkraken
+brew install gnupg
 brew install --cask google-chrome
 brew install --cask google-drive
 brew install --cask inkscape
@@ -30,9 +33,11 @@ brew install mas
 brew install --cask messenger
 brew install --cask microsoft-remote-desktop
 brew install --cask microsoft-teams
+brew install node
 brew install --cask parallels
 brew install --cask postman
 brew install --cask powershell
+brew install ruby
 brew install --cask slack
 brew install --cask visual-studio
 brew install --cask visual-studio-code
@@ -45,6 +50,9 @@ mas install 1561788435 # Usage
 
 # Install Xcode command line tools if necessary
 xcode-select --install || true
+
+# Remove Dock autohide animation delay
+defaults write com.apple.dock autohide-delay -float 0; defaults delete com.apple.dock autohide-time-modifier
 
 # Restart machine
 echo "Software installation completed."
